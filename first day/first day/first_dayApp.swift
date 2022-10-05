b82d30eeb471d1eca5ad355aaf2e2c61486c3479
Eyadazab <dodyazab2008.kw@gmail.com>
2022-10-05 19:17:05 +0300

    added a photo

---
+++ first day/first day/first_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_dayApp.swift
+//  first day
+//
+//  Created by Moahmed Azab on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct first_dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
